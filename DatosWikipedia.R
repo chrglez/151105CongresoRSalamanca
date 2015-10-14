643328a48184087094e18f4335f1510baeb9dfd1
Christian <aramaki2501@gmail.com>
2015-10-14 13:49:38 +0100

    Versión con Wikipediatrend
    https://cran.r-project.org/web/packages/wikipediatrend/vignettes/using-wikipediatrend.html

--- DatosWikipedia.R
+++ DatosWikipedia.R
@@ -1,44 +1,17 @@
-# Descargo y tabulo datos sobre búsquedas en wikipedia
-http://stats.grok.se
+#https://cran.r-project.org/web/packages/wikipediatrend/vignettes/using-wikipediatrend.htmls
 
-## R script
+#☺En caso de que sea necesario instalarlo install.packages("wikipediatrend")
+library(wikipediatrend)
+library(ggplot2)
 
-allURLs <- NULL  # create a place to save the URLs # change this line
+page_views <- wp_trend("main_page")
+ggplot(page_views, aes(x=date, y=count)) +
+      geom_line(size=1.5, colour="steelblue") +
+      geom_smooth(method="loess", colour="#00000000", fill="#001090", alpha=0.1) +
+      scale_y_continuous( breaks=seq(5e6, 50e6, 5e6) ,
+                          label= paste(seq(5,50,5),"M") ) +
+      theme_bw()
 
-for (year in (2008:2014)) {
-      for (month in (1:12)) {
-            
-            if ((year == 2014) && (month > 10)) {
-                  next
-            }
-            
-            theURL <- "http://stats.grok.se/json/en/"
-            theURL <- paste0(theURL, year)
-            
-            if (month < 10) {
-                  theURL <- paste0(theURL, "0")
-            }
-            
-            theURL <- paste0(theURL, month) 
-            theURL <- paste0(theURL, "/Friday")
-            
-            allURLs <- c(allURLs, theURL)  # replace the print command with a
-            # command which adds each URL to
-            # allURLs
-      }
-}
-
-## END OF SCRIPT
-
-library(RCurl)
-rawData <-getURL(allURLs)
-rawData
-
-
-library(RJSONIO)  # Load the JSON parser
-fromJSON(rawData)
-
-parsedData <- fromJSON(rawData)
-parsedData
-parsedData$daily_views
-
+simple_page_views<-page_views[1:2]
+page_views_ts<-as.xts(simple_page_views[2],order.by=as.Date(simple_page_views[1]))
+#Falla porque no reconoce la columna 1 como fecha. Mirar
